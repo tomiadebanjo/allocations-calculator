cb8c3260ff399bf305e3611e84b77cedbf7015ae
Oluwatominiyin Adebanjo <tomiadebanjo@gmail.com>
2020-01-28 11:06:39 +0100

    add unit tests for the allocation service

--- src/app/service/allocation.service.spec.ts
+++ src/app/service/allocation.service.spec.ts
@@ -1,12 +1,72 @@
-import { TestBed } from '@angular/core/testing';
-
 import { AllocationService } from './allocation.service';
 
 describe('AllocationService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  let service: AllocationService;
 
-  it('should be created', () => {
-    const service: AllocationService = TestBed.get(AllocationService);
-    expect(service).toBeTruthy();
+  beforeEach(() => {
+    service = new AllocationService();
+  });
+
+  it('should have no users', done => {
+    service.users$.subscribe(value => {
+      expect(value.length).toBe(0);
+    });
+
+    done();
+  });
+
+  it('should have no allocations', done => {
+    service.allocations$.subscribe(value => {
+      expect(value.length).toBe(0);
+    });
+
+    done();
+  });
+
+  it('should add user when the addUser method is called', done => {
+    service.addUser({ name: 'Tomi', role: 'manager' });
+
+    service.users$.subscribe(value => {
+      expect(value.length).toBe(1);
+      expect(value[0].name).toBe('Tomi');
+    });
+    done();
+  });
+
+  it('should fetch user with provided id', done => {
+    service.addUser({ name: 'Tomi', role: 'manager' });
+
+    const payload = service.fetchUser(1);
+
+    expect(payload.id).toEqual(1);
+    expect(payload.name).toEqual('Tomi');
+    expect(payload.role).toEqual('manager');
+    expect(payload.managerId).toEqual(undefined);
+    expect(payload.staff.length).toEqual(0);
+    done();
+  });
+
+  it('should return total number of allocations for a user', done => {
+    service.addUser({ name: 'Tomi', role: 'manager' });
+    const user = service.fetchUser(1);
+    const allocation = service.totalAllocation(user);
+    expect(allocation).toEqual(30000);
+    done();
+  });
+
+  it('should fetch all users', done => {
+    service.addUser({ name: 'Tomi', role: 'manager', managerId: null });
+    service.addUser({ name: 'JOJI', role: 'manager', managerId: 1 });
+
+    expect(service.fetchAll().length).toEqual(1);
+    done();
+  });
+
+  it('should calculate the allocation total', done => {
+    service.addUser({ name: 'Tomi', role: 'manager', managerId: null });
+    service.addUser({ name: 'JOJI', role: 'manager', managerId: 1 });
+
+    expect(service.calculateAllocationTotal()).toEqual(60000);
+    done();
   });
 });
